f1273cd192a2f851756e38e864b0a6f44cceec6d
flumpyz <natalianata2001@mail.ru>
2022-06-07 17:44:11 +0500

    written arrayService to find the sum of 2 minimum numbers in an array

--- src/Components/InputForm/index.jsx
+++ src/Components/InputForm/index.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import cx from 'classnames';
 import s from './style.module.css';
+import {calculateMinPairInArraySum} from "../../Services/arrayService";
 
 const Index = () => {
     const [inputValue, setInputValue] = useState("");
@@ -9,16 +10,18 @@ const Index = () => {
         setInputValue(event.target.value);
     }
 
-    function handleSubmit() {
+    function handleSubmit(event) {
+        event.preventDefault();
+        calculateMinPairInArraySum(inputValue);
     }
 
     return (
-        <form className={cx(s.inputFormContainer)}>
+        <form className={cx(s.inputFormContainer)} onSubmit={handleSubmit}>
             <h1 className={cx(s.inputFormContainer__title)}>Введите числа массива через запятую</h1>
             <h1 className={cx(s.inputFormContainer__title)}>Input value : {inputValue}</h1>
             <textarea className={cx(s.inputFormContainer__textarea)} value={inputValue} onChange={handleChange}/>
 
-            <input className={cx(s.inputFormContainer__submit)} type="submit" onSubmit={handleSubmit}/>
+            <input className={cx(s.inputFormContainer__submit)} type="submit"/>
         </form>
     );
 };
---
+++ src/Services/arrayService.js
@@ -0,0 +1,51 @@
+export function calculateMinPairInArraySum(arrayInString) {
+    let array = parseStringToNumbersArray(arrayInString);
+
+    let [firstNumber, secondNumber] = findMinimumPairInArray(array);
+    let sum = firstNumber + secondNumber;
+
+    alert (`sum : ${sum}`);
+    return sum;
+}
+
+function findMinimumPairInArray(numbersArray) {
+    if (numbersArray.length < 2) {
+        return;
+    }
+
+    let firstMinNumber = numbersArray[0];
+    let secondMinNumber = numbersArray[1];
+
+    for (let i = 2; i < numbersArray.length; i++) {
+        if (numbersArray[i] < firstMinNumber) {
+            if (firstMinNumber < secondMinNumber) {
+                secondMinNumber = firstMinNumber;
+            }
+            firstMinNumber = numbersArray[i];
+        } else if (numbersArray[i] < secondMinNumber) {
+            if (secondMinNumber < firstMinNumber) {
+                firstMinNumber = secondMinNumber;
+            }
+            secondMinNumber = numbersArray[i];
+        }
+    }
+
+    alert(`first: ${firstMinNumber}  second: ${secondMinNumber}`);
+    return [firstMinNumber, secondMinNumber];
+}
+
+function parseStringToNumbersArray(arrayInString) {
+    let array = arrayInString.split(",");
+    let resultArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!isNaN(Number(array[i])) && array[i].trim() !== '') {
+            resultArray.push(Number(array[i]));
+        }
+    }
+
+    console.log(`get ${resultArray}`);
+
+    return resultArray;
+}
+
